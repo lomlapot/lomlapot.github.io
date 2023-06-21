e96400c6ebea11283f5b480bcbeaf2f7ba0974e5
lomlapot <litvinpetro@gmail.com>
2023-06-21 13:16:27 +1200

    Update main.js scroll to top

--- js/main.js
+++ js/main.js
@@ -8,23 +8,23 @@ $(document).ready(function(){
         $(this).toggleClass("active");
     });
 
-    $(window).load(function(){
+/*    $(window).load(function(){
       $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-    });
+    });     */
 
       // Плавная прокрутка
-  $("a.scrollto").click(function() {
+/*  $("a.scrollto").click(function() {
       var elementClick = $(this).attr("href")
       var destination = $(elementClick).offset().top;
       jQuery("html:not(:animated),body:not(:animated)").animate({
         scrollTop: destination
       }, 1500);
       return false;
-    });
+    });     */
 
   // Кнопка "наверх"
 
-  var btn = $('.scroll-to-top')
+/*  var btn = $('.scroll-to-top')
 
   $(window).scroll(function() {
     if ($(window).scrollTop() > 300) {
@@ -37,5 +37,6 @@ $(document).ready(function(){
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, '30');
   });
+	*/
 });
 
