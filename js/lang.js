1ba371ee85bcbbd081fce14cdae1ff817b1b6d7a
lomlapot <litvinpetro@gmail.com>
2023-09-18 18:14:13 +1200

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -1,1 +1,14 @@
-
+window.onclick = ()=> {
+	const texts = document.querySelectorAll(".menu-item");
+//	console.log (texts)
+	texts.forEach(text=> {
+		const value = text.getAttribute("value");
+		const arr = value.split("#");
+		const radios = document.getElementsByName('toggle');
+		for (let i = 0; i < arr.length ; i++) {
+			if (radios[i].checked) {
+				text.innerHTML = arr[i];
+			}
+		}
+	})
+}
