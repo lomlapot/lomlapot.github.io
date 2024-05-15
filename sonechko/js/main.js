d904d03be8fecfd1512f7bbec8f622b765c1b1ee
lomlapot <litvinpetro@gmail.com>
2024-05-16 10:35:36 +1200

    gradent

--- sonechko/js/main.js
+++ sonechko/js/main.js
@@ -1,34 +1,29 @@
-if($(window).width() > 1024)
-{
-    $('body').parallax({
-        'elements': [
-            
-           
-            {
-                'selector': '.container',
-                'properties': {
-                    'x': {
-                        'left': {
-                            'initial':0,
-                            'multiplier': 0.03,
-                            'unit': 'px',
-                            'invert': false
-                        }
-                    },
-                    'y': {
-                        'top': {
-                            'initial': 0,
-                            'multiplier': 0.03,
-                            'unit': 'px',
-                            'invert': false
-                        }
-                    }
-                }
+if ($(window).width() > 1024) {
+  $("body").parallax({
+    elements: [
+      {
+        selector: ".container",
+        properties: {
+          x: {
+            left: {
+              initial: 0,
+              multiplier: 0.03,
+              unit: "px",
+              invert: false,
             },
-           
-        ]
-    });
+          },
+          y: {
+            top: {
+              initial: 0,
+              multiplier: 0.03,
+              unit: "px",
+              invert: false,
+            },
+          },
+        },
+      },
+    ],
+  });
 } else {
-   // change functionality for larger screens
+  // change functionality for larger screens
 }
- 
