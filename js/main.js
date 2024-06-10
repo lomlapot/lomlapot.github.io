d83c44fd3cf952d317f24d6605601aa1b9bf5dc1
lomlapot <litvinpetro@gmail.com>
2024-06-11 11:04:31 +1200

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-//$(document).ready(function(){	
-	//"use strict"
-	 // -------   Active Mobile Menu-------- 
-          //-----------no-jquery------------------
-const menu_toggle=document.querySelector(".menu-toggle");
-const menu =document.querySelector(".menu");
+//$(document).ready(function(){
+//"use strict"
+// -------   Active Mobile Menu--------
+//-----------no-jquery------------------
+const menu_toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
 
-menu_toggle.addEventListener("click", function(e) {
-	menu.classList.toggle("hide");
-	this.classList.toggle("active");	
+menu_toggle.addEventListener("click", function (e) {
+  menu.classList.toggle("hide");
+  this.classList.toggle("active");
 });
 
-  /*  $(".menu-toggle").on('click', function(e){
+/*  $(".menu-toggle").on('click', function(e){
         e.preventDefault();
         $(".menu").toggleClass('hide');
         $(this).toggleClass("active");
@@ -18,7 +18,7 @@ menu_toggle.addEventListener("click", function(e) {
 /*    $(window).load(function(){ $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
     });     */
 
-      // Плавная прокрутка через css
+// Плавная прокрутка через css
 /*  $("a.scrollto").click(function() {
       var elementClick = $(this).attr("href")
       var destination = $(elementClick).offset().top;
@@ -28,7 +28,7 @@ menu_toggle.addEventListener("click", function(e) {
       return false;
     });     */
 
-  // Кнопка "наверх" через intersection observer
+// Кнопка "наверх" через intersection observer
 
 /*  var btn = $('.scroll-to-top')
 
@@ -44,41 +44,44 @@ menu_toggle.addEventListener("click", function(e) {
     $('html, body').animate({scrollTop:0}, '30');
   });
 	*/
-	let options = {
-	   rootMargin:'0px',
-	   threshold:0.3 
-        }
+let options = {
+  rootMargin: "0px",
+  threshold: 0.3,
+};
 
-	let target = document.querySelector("#newborn");
-	let arrow = document.querySelector(".scroll-to-top");	
-        const animationM = document.querySelector(".newmia")
-    	const observer = new IntersectionObserver(entries=>{
-         entries.map(entry=>{	
-	   console.log(entry.target);
-        })  
-         arrow.classList.toggle('show');
-	 animationM.classList.toggle("mia-anim");	
-      },options );
+let target = document.querySelector("#newborn");
+let arrow = document.querySelector(".scroll-to-top");
+const animationM = document.querySelector(".newmia");
+const observer = new IntersectionObserver((entries) => {
+  entries.map((entry) => {
+    console.log(entry.target);
+  });
+  arrow.classList.toggle("show");
+  animationM.classList.toggle("mia-anim");
+}, options);
 observer.observe(target);
-//----------------------------------------------	
-const videoObserver =new IntersectionObserver((entries)=>{
-	entries.forEach((entry)=>{
-		const video =entry.target;
-		if(video.currentTiime===0) return;
-		if(!entry.isIntersecting){
-			video.pause();
-			video.currentTime=0;
-		}
-	})	
-},{threshold:[0.1,0.9]}
+//----------------------------------------------
+const videoObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const video = entry.target;
+      if (video.currentTiime === 0) return;
+      if (!entry.isIntersecting) {
+        video.pause();
+        video.currentTime = 0;
+      }
+    });
+  },
+  { threshold: [0.1, 0.9] }
 );
-	document.querySelectorAll("video")
-	.forEach((video)=> videoObserver.observe(video));
+document
+  .querySelectorAll("video")
+  .forEach((video) => videoObserver.observe(video));
 //});
 //--------------------------------------------
 
 const sections = document.querySelectorAll("section");
-const links = document.querySelectorAll("li a");
+const links = document.querySelectorAll(" a");
 
 const callback = (entries) => {
   entries.forEach((entry) => {
@@ -86,9 +89,7 @@ const callback = (entries) => {
       links.forEach((link) => link.classList.remove("active-item"));
 
       const activeId = entry.target.id;
-      const activeLink = document.querySelector(
-        `a[href="#${activeId}"]`
-      );
+      const activeLink = document.querySelector(`a[href="#${activeId}"]`);
 
       if (activeLink) {
         activeLink.classList.add("active-item");
@@ -98,7 +99,7 @@ const callback = (entries) => {
 };
 
 const sectionObserver = new IntersectionObserver(callback, {
-  threshold: [0.2, 0.8]
+  threshold: [0.2, 0.8],
 });
 
 sections.forEach((sec) => {
