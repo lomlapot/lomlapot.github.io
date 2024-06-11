bdbaa9b58dc358b9ad7cb3f92fb196b2c813860e
lomlapot <litvinpetro@gmail.com>
2024-06-11 13:22:47 +1200

    stars

--- sonechko/js/stars.js
+++ sonechko/js/stars.js
@@ -1,8 +1,8 @@
-for (let index = 0; index < 290; index++) {
+for (let index = 0; index < 390; index++) {
   const star = document.createElement("span");
   star.innerText = ".";
   star.classList = "star";
   star.style.setProperty("top", `${Math.random() * 100}%`);
   star.style.setProperty("left", `${Math.random() * 100}%`);
-  document.querySelector(".container").append(star);
+  document.querySelector(".stars").append(star);
 }
