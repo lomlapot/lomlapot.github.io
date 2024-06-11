7da4976d09703570153afcbcb2ab904532e6096d
lomlapot <litvinpetro@gmail.com>
2024-06-11 13:22:25 +1200

    stars

--- sonechko/js/main.js
+++ sonechko/js/main.js
@@ -22,6 +22,28 @@ if ($(window).width() > 1024) {
           },
         },
       },
+
+      {
+        selector: ".stars",
+        properties: {
+          x: {
+            left: {
+              initial: 0,
+              multiplier: 0.04,
+              unit: "px",
+              invert: true,
+            },
+          },
+          y: {
+            top: {
+              initial: 0,
+              multiplier: 0.03,
+              unit: "px",
+              invert: true,
+            },
+          },
+        },
+      },
     ],
   });
 } else {
